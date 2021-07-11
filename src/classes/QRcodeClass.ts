fc77f9595f13faa78ced0864b08278adf0cba15e
OwenPotent <sheanowen3@gmail.com>
2021-07-11 12:09:02 +0800

    Fixed QRCode functions

--- src/classes/QRcodeClass.ts
+++ src/classes/QRcodeClass.ts
@@ -1,20 +1,44 @@
-let parametersJson = {
-    "size": 250, // Size of Qr Code
-    "backgroundColor": "38-38-38", // Background Color Of Qr Code (In RGB)
-    "qrColor": "255-255-255", // Color of Qr Code (In RGB)
-    "padding": 2, // Padding 
-    "data": "google.com"
-};
+import { parameters } from "../types/enums"
 
 export class QRCode {
-    static genQRCode(data: string) {
 
-        parametersJson.data = "google.com" || data
+    private _data = "google.com"
 
-        let parameters = `size=${parametersJson.size}&bgcolor=${parametersJson.backgroundColor}&color=${parametersJson.qrColor}&qzone=${parametersJson.padding}&data=${parametersJson.data}`
-        
-        let img = `https://api.qrserver.com/v1/create-qr-code/?${parameters}`
+    private parametersJson = {
+        "size": 250, // Size of Qr Code
+        "backgroundColor": "38-38-38", // Background Color Of Qr Code (In RGB)
+        "qrColor": "255-255-255", // Color of Qr Code (In RGB)
+        "padding": 2, // Padding 
+        "data": this._data
+    };
+
+    constructor(parameters: parameters) {
+        this._data = this._data || parameters.data || this.parametersJson.data
+    }
+
+    /**
+     * 
+     * @returns the QRcode link
+     */
+
+    public genQR() {
+        let param = `size=${this.parametersJson.size}&bgcolor=${this.parametersJson.backgroundColor}&color=${this.parametersJson.qrColor}&qzone=${this.parametersJson.padding}&data=${this._data}`
+
+        let img = `https://api.qrserver.com/v1/create-qr-code/?${param}`
 
         return img
     }
+
+    // Unused code cause of error
+
+    // static genQRCode(data: string) {
+
+    //     this.parametersJson.data = "google.com" || data
+
+    //     let parameters = `size=${this.parametersJson.size}&bgcolor=${this.parametersJson.backgroundColor}&color=${this.parametersJson.qrColor}&qzone=${this.parametersJson.padding}&data=${this.parametersJson.data}`
+        
+    //     let img = `https://api.qrserver.com/v1/create-qr-code/?${parameters}`
+
+    //     return img
+    // }
 }
